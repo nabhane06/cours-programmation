c4c14164ef20587f5f5ce815b54b9f4b923fcff9
philippe goux <goux.phil@gmail.com>
2025-07-20 17:28:29 +0200

    condition switch

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-let menu = "dessert";
+let menu = "hors-d'oeuvre";
 
-if (menu === "dessert") {
-  console.log("gateau au chocolat");
-} else if (menu === "hors-d'oeuvre") {
-  console.log("tomate mozza");
-} else if (menu === "plat principal") {
-  console.log("gratin dauphinois");
-} else {
-  console.log("quiche");
+// if (menu === "dessert") {
+//   console.log("gateau au chocolat");
+// } else if (menu === "hors-d'oeuvre") {
+//   console.log("tomate mozza");
+// } else if (menu === "plat principal") {
+//   console.log("gratin dauphinois");
+// } else {
+//   console.log("quiche");
+// }
+
+switch (menu) {
+  case "dessert":
+    console.log("gateau au chocolat");
+    break;
+  case "hors-d'oeuvre":
+    console.log("tomate mozza");
+    break;
+  case "plat principal":
+    console.log("gratin dauphinois");
+    break;
+  default:
+    console.log("quiche");
 }
