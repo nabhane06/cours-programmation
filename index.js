2a789f209759d7dcd2bac56c92a2c196d121560e
philippe goux <goux.phil@gmail.com>
2025-07-20 17:52:08 +0200

    do while

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const friends = ["coco", "jaco", "pedro"];
+const friends = ["coco", "jaco"];
 let count = 0;
 
 while (count < friends.length) {
   console.log(friends[count]);
   count = count + 1;
 }
+
+// do {
+//   // console.log();
+// } while (count < friends.length);
 
 console.log("fin");
