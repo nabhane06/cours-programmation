aac1df746d1b811b332c84bee3b9281ee00caa79
philippe goux <goux.phil@gmail.com>
2025-07-22 23:18:40 +0200

    creation d'objet dans un objet (imbrication)

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const coco = {
   name: "Coco",
   age: 29,
   email: "coco@gmail.fr",
+  address: {
+    street: "64 place du centenaire",
+    city: "la conception",
+    zipcode: "j0t1m0",
+  },
 };
 
-console.log(coco.name); // Coco
-console.log(coco.age); // 29
-
-const imprimante = {
-  name: "epson",
-  couleur: "noir",
-  forme: "carré",
-};
-
-console.log(imprimante.name);
-console.log(imprimante.couleur);
-console.log(imprimante.forme);
+console.log(coco.name);
+console.log(coco.address.city);
+console.log(coco.name + " habite a " + coco.address.city);
