e52eb6e6dec46978518eb45e2fb8272ead16dd5e
philippe goux <goux.phil@gmail.com>
2025-07-22 22:44:51 +0200

    les 3 boucle for

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ for (let i = 0; i < friends.length; i = i + 1) {
   console.log(friends[i]);
 }
 
+for (let friend of friends) {
+  console.log(friend);
+}
+
+for (let index in friends) {
+  console.log(index);
+}
+
 console.log("fin");
