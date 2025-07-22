e42cbad519ff48cb806671db97537e403a2b2604
philippe goux <goux.phil@gmail.com>
2025-07-22 23:42:38 +0200

    retour de la fonction

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ function add(nb1, nb2) {
   console.log("fonction d'addition");
   console.log(nb1 + nb2 + nb1);
   console.log("fin");
+  return nb1 + nb2 + nb1;
 }
 
-add(10, 5);
+const result = add(10, 5);
 add(3, 9);
 add(-6, 8.5);
+console.log("retour du 1er appel => " + result);
