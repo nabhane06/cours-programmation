158e12cefb51f2ef0f7cf2f628b9b4a36e45b2bb
philippe goux <goux.phil@gmail.com>
2025-07-22 23:31:16 +0200

    creation de fonction et execution de la fonction (3)

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const coco = {
-  name: "Coco",
-  age: 29,
-  email: "coco@gmail.fr",
-  address: {
-    street: "64 place du centenaire",
-    city: "la conception",
-    zipcode: "j0t1m0",
-  },
-};
+function add() {
+  console.log("fonction d'addition");
+  console.log(2 + 3);
+  console.log("fin");
+}
 
-console.log(coco.name);
-console.log(coco.address.city);
-console.log(coco.name + " habite a " + coco.address.city);
+add();
+add();
+add();
