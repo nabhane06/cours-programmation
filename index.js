1cc79b4bb7692fa334778b4c03c6618c38babe9f
philippe goux <goux.phil@gmail.com>
2025-07-29 23:15:22 +0200

    fonction findIndex boucle

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const numbers = [78, 5, -2, 1000, 76, 2, 1, -76, 8887776, 56];
 
-const result = numbers.filter(display);
-console.log("Result -->", result); // Result --> [78, 1000, 76, 88877776, 56]
+const result = numbers.findIndex(display);
+console.log("Result -->", result); // Result --> 2
 
 // Déclarations de fonctions
 function display(nb, index) {
   console.log("Valeur:", nb);
   console.log("Position:", index);
 
-  return nb >= 50;
+  return nb <= 4;
 }
