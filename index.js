e89edd3241097f7b67c46e2cf0ccf17764255d67
philippe goux <goux.phil@gmail.com>
2025-07-31 20:33:39 +0200

    exercice car : fonction flecher

--- index.js
+++ index.js
@@ -1,51 +1,13 @@
-const users = [
-  {
-    id: 1,
-    name: "Coco",
-    email: "coco@mail.fr",
-    role: "ADMIN",
-  },
-  {
-    id: 2,
-    name: "Jaco",
-    email: "jaco@mail.fr",
-    role: "EDITOR",
-  },
-  {
-    id: 3,
-    name: "Pedro",
-    email: "pedro@mail.fr",
-    role: "SUPER_ADMIN",
-  },
-  {
-    id: 4,
-    name: "Franco",
-    email: "franco@mail.fr",
-    role: "ADMIN",
-  },
-  {
-    id: 5,
-    name: "Fernando",
-    email: "fernando@mail.fr",
-    role: "SUPER_ADMIN",
-  },
-];
+const car = {
+  name: "porshe",
+  model: "macane",
+  year: "2025",
+  finition: "sports",
+  start: () => "la voiture est en train de rouler",
+  accelerate: () => "la voiture accelere",
+  stop: () => "la voiture s'arrete",
+};
 
-// CONSIGNE :
-// extraire tous les rôles désirés --> [].filter()
-// programme configurable depuis le terminal (variable d'environnement)
-// Exemple: ROLE=ADMIN node . OU ROLE=EDITOR node .
-
-const usersFiltered = users.filter(getRole);
-
-for (let user of usersFiltered) {
-  display(user.name, user.email);
-}
-
-function getRole(user) {
-  return user.role === process.env.ROLE;
-}
-
-function display(userName, userEmail) {
-  console.log("name:", userName, "(" + userEmail + ")");
-}
+console.log(car.start());
+console.log(car.accelerate());
+console.log(car.stop());
