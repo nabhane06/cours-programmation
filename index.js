6a1c83f24d84ad3dd474887b4fcbafb71bd18efe
philippe goux <goux.phil@gmail.com>
2025-07-31 20:48:23 +0200

    exercice car : etat de l'objet

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const car = {
   model: "macane",
   year: "2025",
   finition: "sports",
+  started: false,
   start: () => "la voiture est en train de rouler",
   accelerate: () => "la voiture accelere",
   stop: () => "la voiture s'arrete",
 };
 
 console.log(car.start());
-console.log(car.accelerate());
-console.log(car.stop());
+car.started = true;
+
+if (car.started) {
+  console.log(car.accelerate());
+  console.log(car.stop());
+  car.started = false;
+}
