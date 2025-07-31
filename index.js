0f752bb79929aaf879460f3f3073683d0a635659
philippe goux <goux.phil@gmail.com>
2025-07-31 21:18:41 +0200

    exercice car : gestion des incoherances

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
+// Dev 1 (celui qui crée l'objet car...)
 const car = {
   name: "porshe",
   model: "macane",
   year: "2025",
   finition: "sports",
   started: false,
-  start: () => "la voiture est en train de rouler",
+  start: function () {
+    if (!this.started) {
+      this.started = true;
+      console.log("la voiture démarre et se met à rouler");
+    } else {
+      console.log("Attention: la voiture est déjà en train de rouler");
+    }
+  },
   accelerate: () => "la voiture accelere",
-  stop: () => "la voiture s'arrete",
+  stop: function () {
+    if (this.started) {
+      this.started = false;
+      console.log("la voiture s'arrete");
+    } else {
+      console.log("Attention: la voiture est deja arretée");
+    }
+  },
 };
 
-console.log(car.start());
-car.started = true;
-
-if (car.started) {
-  console.log(car.accelerate());
-  console.log(car.stop());
-  car.started = false;
-}
+// Dev 2 (celui qui utilise l'objet car)
+car.start();
+car.accelerate();
+car.start();
+car.stop();
