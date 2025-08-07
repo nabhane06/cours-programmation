396d5bc4417117d6bf79efde183199936b294b51
philippe goux <goux.phil@gmail.com>
2025-08-07 20:15:53 +0200

    ex-game : creation et affichage d'une grille

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("je veux faire du velos");
+const TILE = ".";
+
+const grid = [
+  [TILE, TILE, TILE, TILE, TILE],
+  [TILE, TILE, TILE, TILE, TILE],
+  [TILE, TILE, TILE, TILE, TILE],
+  [TILE, TILE, TILE, TILE, TILE],
+  [TILE, TILE, TILE, TILE, TILE],
+];
+
+console.log(grid[0].join(" "));
+console.log(grid[1].join(" "));
+console.log(grid[2].join(" "));
+console.log(grid[3].join(" "));
+console.log(grid[4].join(" "));
