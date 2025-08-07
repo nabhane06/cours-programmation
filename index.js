12f2784fad10ada7adbd73fa885781fcddc34968
philippe goux <goux.phil@gmail.com>
2025-08-07 21:01:05 +0200

    ex-game : creation et generation dynamic d'une grille

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const TILE = ".";
 
 const grid = [
-  [TILE, TILE, TILE, TILE, TILE],
-  [TILE, TILE, TILE, TILE, TILE],
-  [TILE, TILE, TILE, TILE, TILE],
-  [TILE, TILE, TILE, TILE, TILE],
-  [TILE, TILE, TILE, TILE, TILE],
-];
+  [TILE, TILE, TILE], // 0 length --> 3
+  [TILE, TILE, TILE], // 1 length --> 3
+  [TILE, TILE, TILE], // 2 length --> 3
+]; // length --> 3
 
-console.log(grid[0].join(" "));
-console.log(grid[1].join(" "));
-console.log(grid[2].join(" "));
-console.log(grid[3].join(" "));
-console.log(grid[4].join(" "));
+let str = "";
+
+for (let i = 0; i < grid.length; i++) {
+  for (let j = 0; j < grid[i].length; j++) {
+    str += TILE;
+  }
+  str += "\n";
+}
+
+console.log(str);
