af309fcff2708365c7e0c359c265dc058c429322
philippe goux <goux.phil@gmail.com>
2025-08-19 18:04:22 +0200

    ex-game :  fonction de creationde la carte

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const TILE = " . ";
 const SIZE = 5;
 
 const grid = [];
-let map = "";
 
-// CREER UNE MAP CARREE
-// 1/ Créer une première ligne de tuiles en fonction d'une taille donnée
-// 2/ Après chaque ligne de tuiles, il faut sauter une ligne (\n)
+createGrid();
 
-for (let i = 0; i < SIZE; i++) {
-  // Création d'une ligne
+function createGrid() {
+  let map = "";
+
   for (let i = 0; i < SIZE; i++) {
-    map += TILE;
+    // Création d'une ligne
+    for (let i = 0; i < SIZE; i++) {
+      map += TILE;
+    }
+    // Ajout d'un saut de ligne
+    map += "\n";
   }
-  // Ajout d'un saut de ligne
-  map += "\n";
-}
 
-console.log(map);
+  // Affichage de la map
+  console.log(map);
+}
