22aa022ff88a321d83c044ece65c52033a092547
philippe goux <goux.phil@gmail.com>
2025-08-20 01:07:26 +0200

    ex-game : placement des tuiles

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const TILE = {
 const SIZE = 5;
 const grid = [];
 
+// Si random est inférieur à 0.1 --> Tuile enemie
+// Si random est inférieur à 0.2 --> Tuile trésor
+// Si random est inférieur à 0.3 --> Tuile piège
+// Dans tous les autres cas --> Tuile vide
+
 createGrid();
 displayGrid();
 
@@ -17,13 +22,24 @@ function createGrid() {
 
     // Création d'une ligne
     for (let j = 0; j < SIZE; j++) {
-      grid[i].push(TILE.EMPTY);
+      const random = Math.random();
+      if (random < 0.1) {
+        grid[i].push(TILE.ENEMY);
+      } else if (random < 0.2) {
+        grid[i].push(TILE.TREASURE);
+      } else if (random < 0.3) {
+        grid[i].push(TILE.TRAP);
+      } else {
+        grid[i].push(TILE.EMPTY);
+      }
     }
   }
 }
 
 function displayGrid() {
+  console.log("");
   for (let row of grid) {
     console.log(row.join(""));
   }
+  console.log("");
 }
